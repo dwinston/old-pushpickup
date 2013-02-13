5481fc78e2884984375f320ef65d5e061b1f80d4
Donny Winston <dwinston@alum.mit.edu>
2013-02-12 20:32:30 -0800

    Add a full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,21 +3,24 @@ require 'spec_helper'
 describe "StaticPages" do
 
   subject { page }
+  let(:base_title) { 'Push Pickup' }
   
   describe "Home page" do
     before { visit '/static_pages/home' }
-    it { should have_content 'Push Pickup' }
-    it { should have_selector 'title', text: 'Push Pickup | Home' }
+    it { should have_selector 'h1', text: 'Push Pickup' }
+    it { should have_selector 'title', text: base_title }
   end 
   
   describe "Help page" do
     before { visit '/static_pages/help' }
-    it { should have_content 'Help' }
+    it { should have_selector 'h1', text: 'Help' }
+    it { should have_selector 'title', text: "#{base_title} | Help" }
   end
 
   describe "About page" do
     before { visit '/static_pages/about' }
-    it { should have_content 'About Us' }
+    it { should have_selector 'h1', text: 'About Us' }
+    it { should have_selector 'title', text: "#{base_title} | About Us" }
   end
 
 end
