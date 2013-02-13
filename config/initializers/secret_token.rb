5cc5dbf2722d0fcd048ae81e55c07f99c72c4563
Donny Winston <dwinston@alum.mit.edu>
2013-02-12 17:37:46 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PushPickup::Application.config.secret_token = '2b4d42cb031068df280b63aafe63780397c6f079c03c4937a851868b51007d8408e6961277f53ca791eec7f54103d29436beeceabc148dc163bbc65565d8918b'
