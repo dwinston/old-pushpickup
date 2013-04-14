c52d4f62c6d6ac5c2fad8dc345ff59e5971a5420
Donny Winston <dwinston@alum.mit.edu>
2013-04-14 12:19:27 -0700

    Add content to static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -63,15 +63,15 @@ describe "StaticPages" do
 
   describe "About page" do
     before { visit about_path }
-    let(:heading) { 'About Us' }
-    let(:page_title) { 'About Us' }
+    let(:heading) { 'About ' + full_title('') }
+    let(:page_title) { 'About ' + full_title('') }
     it_should_behave_like "all static pages"
   end
 
   it "should have the right links on the layout" do
     visit root_path
     click_link "About"
-    page.should have_selector 'title', text: full_title('About Us')
+    page.should have_selector 'title', text: full_title('About ' + full_title(''))
     click_link "Help"
     page.should have_selector 'title', text: full_title('Help')
     click_link "Home"
